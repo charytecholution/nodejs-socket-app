22638835916d70be9607f2b1dcdd151595d39d34
charytecholution <chary@techolution.com>
2020-09-14 16:05:53 +0530

    Node Socket app

---
+++ server.js
@@ -0,0 +1,51 @@
+// server.js
+
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const PORT = 3000;
+server.listen(PORT);
+console.log("Server is running");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+const connections = [];
+
+io.sockets.on("connection", socket => {
+  connections.push(socket);
+  console.log(" %s sockets is connected", connections.length);
+
+  socket.on("disconnect", () => {
+    connections.splice(connections.indexOf(socket), 1);
+  });
+socket.on("sending message", message => {
+  console.log("Message is received :", message);
+
+  io.sockets.emit("new message", { message: message });
+});
+});
+var ip = require("ip");
+console.log( ip.address() );
+'use strict';
+
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+
+            results[name].push(net.address);
+        }
+    }
+}
